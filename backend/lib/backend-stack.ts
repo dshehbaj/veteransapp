864599c5a88ec3e11f1cb5e558e52e74986fd168
dshehbaj <shehbaj.dhillon@gmail.comaa>
2022-03-05 12:52:32 -0800

    Update AWS CDK Entry Point Files

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,16 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import CognitoUserPool from './cognito/cognito-user-pool';
+import RDSDatabase from './rds/pgsql-db';
 
 export class BackendStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CognitoUserPool(this, 'veterans-app--user-pool', props);
+    new RDSDatabase(this, 'veterans-app-rds-instance', props);
   }
 }
+
