00783005e813a99727ae3c93c1598ba9eaa8bf8c
dshehbaj <shehbaj.dhillon@gmail.comaa>
2022-03-05 15:18:46 -0800

    Remove RDS DB

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,7 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import CognitoUserPool from './cognito/cognito-user-pool';
-import RDSDatabase from './rds/pgsql-db';
 import APIGateway from './apigateway/apigateway';
 
 export class BackendStack extends Stack {
@@ -9,8 +8,6 @@ export class BackendStack extends Stack {
     super(scope, id, props);
     const cognitoUserPool =
       new CognitoUserPool(this, 'veterans-app--user-pool', props);
-    const rdsDatabase =
-      new RDSDatabase(this, 'veterans-app--rds-instance', props);
     const apiGateway =
       new APIGateway(this, 'veterans-app--api-gateway', props);
 
