fad2c4e61346d3d2bc57a238d20d0989309c273a
Jack Nelson <jmn3lson@gmail.com>
2022-03-05 18:56:10 -0800

    buttons and stack added

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,6 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Profile from './Profile';
+import { Button } from 'react-native-paper';
+import Leader from './Leaderboard';
+import AboutUs from './AboutUs';
 
 const Stack = createStackNavigator();
 
@@ -10,9 +14,13 @@ export default function App() {
     <NavigationContainer>
       <StatusBar style="auto" />
       <Stack.Navigator initialRouteName="Dummy2">
-        <Stack.Screen name="Dummy1" component={DummyScreen1} />
+        <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="Dummy2" component={DummyScreen2} />
+        <Stack.Screen name="Leader" component={Leader} />
+        <Stack.Screen name="AboutUs" component={AboutUs} />
     
+       
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -23,17 +31,39 @@ const DummyScreen1 = () => {
     <View style={styles.container}>
       <Text>This is Dummy Screen 1</Text>
       <StatusBar style="auto" />
+      
     </View>
   );
 };
 
-const DummyScreen2 = () => {
+
+
+const DummyScreen2 = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>This is Dummy Screen 2</Text>
+      <Button
+  
+        onPress={() => navigation.navigate('Profile')}
+      >
+      Profile
+      </Button>
+      <Button
+  
+        onPress={() => navigation.navigate('Leader')}
+      >
+      Leaderboard
+      </Button>
+      <Button
+  
+        onPress={() => navigation.navigate('AboutUs')}
+      >
+      Our Team
+      </Button>
     </View>
   );
 };
+
 
 const styles = StyleSheet.create({
   container: {
